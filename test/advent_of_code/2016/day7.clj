95607725ba21af6b4a60cb990d10e889fc50dab9
Mikelis <mikelis.vindavs@gmail.com>
2017-12-04 20:05:04 +0200

    2016.7

---
+++ test/advent_of_code/2016/day7.clj
@@ -0,0 +1,15 @@
+(ns advent-of-code.2016.day7
+  (:require [clojure.test :refer :all]))
+
+
+(deftest test-solve1
+  (is (= (solve1 "abba[mnop]qrst") 1))
+  (is (= (solve1 "abcd[bddb]xyyx") 0))
+  (is (= (solve1 "aaaa[qwer]tyui") 0))
+  (is (= (solve1 "ioxxoj[asdfgh]zxcvbn") 1)))
+
+(deftest test-solve2
+  (is (= (solve2 "aba[bab]xyz") 1))
+  (is (= (solve2 "xyx[xyx]xyx") 0))
+  (is (= (solve2 "aaa[kek]eke") 1))
+  (is (= (solve2 "zazbz[bzb]cdb") 1)))

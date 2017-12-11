66e72d4082903d7ddc54531bd2292a672e5a8f31
Mikelis <mikelis.vindavs@gmail.com>
2017-12-11 08:01:56 +0200

    2017.11

---
+++ test/advent_of_code/2017/day11_test.clj
@@ -0,0 +1,13 @@
+(ns advent-of-code.2017.day11-test
+  (:require [clojure.test :refer :all]
+            [advent-of-code.2017.day11 :refer :all]))
+
+(deftest test-solve1
+  (is (= (solve1 "ne,ne,ne") 3))
+  (is (= (solve1 "ne,ne,sw,sw") 0))
+  (is (= (solve1 "ne,ne,s,s") 2))
+  (is (= (solve1 "se,sw,se,sw,sw") 3))
+  (is (= (solve1 input) 773)))
+
+(deftest test-solve2
+  (is (= (solve2 input) 1560)))

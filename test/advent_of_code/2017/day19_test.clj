14965016abbdf104506b3ac869873c1abefc3ca1
Mikelis <mikelis.vindavs@gmail.com>
2017-12-19 14:18:33 +0200

    2017.19

---
+++ test/advent_of_code/2017/day19_test.clj
@@ -0,0 +1,11 @@
+(ns advent-of-code.2017.day19-test
+  (:require [clojure.test :refer :all]
+            [advent-of-code.2017.day19 :refer :all]))
+
+(deftest test-solve1
+  (is (= (solve1 test-input) "ABCDEF"))
+  (is (= (solve1 input) "MOABEUCWQS")))
+
+(deftest test-solve2
+  (is (= (solve2 test-input) 38))
+  (is (= (solve2 input) 18058)))

cb00eaf61ad9355d4c16f5086b010825aa7a4dff
Miķelis Vindavs <mikelis.vindavs@gmail.com>
2020-11-17 23:44:17 +0200

    2015.9

---
+++ test/advent_of_code/2015/day09_test.clj
@@ -0,0 +1,13 @@
+(ns advent-of-code.2015.day09-test
+  (:require [clojure.test :refer :all]
+            [advent-of-code.2015.day09 :refer :all]))
+
+(def test-input "London to Dublin = 464\nLondon to Belfast = 518\nDublin to Belfast = 141")
+
+(deftest test-solve1
+  (is (= (solve1 test-input) 605))
+  (is (= (solve1 input) 117)))
+
+(deftest test-solve2
+  (is (= (solve2 test-input) 982))
+  (is (= (solve2 input) 909)))

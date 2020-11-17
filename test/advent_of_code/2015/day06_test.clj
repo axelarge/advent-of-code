30b4f2de521367aa8cbc3881aab23a76a821a35a
Miķelis Vindavs <mikelis.vindavs@gmail.com>
2020-11-17 19:58:42 +0200

    2015.6

---
+++ test/advent_of_code/2015/day06_test.clj
@@ -0,0 +1,9 @@
+(ns advent-of-code.2015.day06-test
+  (:require [clojure.test :refer :all]
+            [advent-of-code.2015.day06 :refer :all]))
+
+(deftest ^:slow test-solve1
+  (is (= (solve1 input) 377891)))
+
+(deftest test-solve2
+  (is (= (solve2 input) 14110788)))

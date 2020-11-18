b60b193cb2632999af4c45a745e6e1b39feb064e
Miķelis Vindavs <mikelis.vindavs@gmail.com>
2020-11-18 03:24:56 +0200

    2015.12

---
+++ src/advent_of_code/2015/day12.clj
@@ -0,0 +1,21 @@
+(ns advent-of-code.2015.day12
+  (:require [advent-of-code.support :refer :all]
+            [clojure.string :as str]))
+
+(def input (get-input 2015 12))
+
+(defn solve1 [input]
+  (->> input
+       (re-seq #"-?\d+")
+       (map parse-int)
+       (reduce +)))
+
+(defn solve2 [input]
+  (->> (str/replace input #":" "")
+       (read-string)
+       (tree-seq coll?
+                 #(when-not (and (map? %)
+                                 (some #{"red"} (vals %)))
+                    (seq %)))
+       (filter number?)
+       (reduce +)))

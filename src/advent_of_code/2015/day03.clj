ec6e4b164a4c738619daaffa4ae9c31c81e35be7
Miķelis Vindavs <mikelis.vindavs@gmail.com>
2020-11-30 23:00:04 +0200

    use take-nth

--- src/advent_of_code/2015/day03.clj
+++ src/advent_of_code/2015/day03.clj
@@ -1,26 +1,18 @@
 (ns advent-of-code.2015.day03
-  (:require [advent-of-code.support :refer :all]
-            [clojure.string :as str]))
+  (:require [advent-of-code.support :refer :all]))
 
 (def input (get-input 2015 3))
 
 (defn positions [moves]
   (->> moves
        (map {\^ [0 -1] \v [0 1] \< [-1 0] \> [1 0]})
-       (reductions (partial mapv +)
-                   [0 0])))
-
-(defn alternate [moves]
-  (apply concat (partition 1 2 moves)))
+       (reductions (partial mapv +) [0 0])))
 
 (defn solve1 [input]
-  (->> input
-       (positions)
-       (set)
-       (count)))
+  (count (set (positions input))))
 
 (defn solve2 [input]
-  (->> (concat (positions (alternate input))
-               (positions (alternate (rest input))))
+  (->> (concat (positions (take-nth 2 input))
+               (positions (take-nth 2 (rest input))))
        (set)
        (count)))

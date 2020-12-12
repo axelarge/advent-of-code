969af52a84e08c9d06c2f9ebfee85b350b993eb8
Miķelis Vindavs <mikelis.vindavs@gmail.com>
2020-12-12 13:39:39 +0200

    clean up

--- src/advent_of_code/2020/day12.py
+++ src/advent_of_code/2020/day12.py
@@ -6,33 +6,23 @@ MOVE = dict(
     W=lambda x, y, n: (x - n, y),
 )
 
-heading = 'E'
-x, y = 0, 0
-for c, n in data:
-    if c == "F":
-        c = heading
-    if c == "R":
-        for _ in range(n // 90):
-            heading = dict(E="S", S="W", W="N", N="E")[heading]
-    elif c == "L":
-        for _ in range(n // 90):
-            heading = dict(E="N", S="E", W="S", N="W")[heading]
-    else:
-        x, y = MOVE[c](x, y, n)
-print(abs(x) + abs(y))
+def solve(dx, dy, part):
+    x, y = 0, 0
+    for c, n in data:
+        if c == "F":
+            x += dx * n
+            y += dy * n
+        elif c == "R":
+            for _ in range(n // 90):
+                dx, dy = -dy, dx
+        elif c == "L":
+            for _ in range(n // 90):
+                dx, dy = dy, -dx
+        elif part == 1:
+            x, y = MOVE[c](x, y, n)
+        else:
+            dx, dy = MOVE[c](dx, dy, n)
+    return abs(x) + abs(y)
 
-x, y = 0, 0
-wpx, wpy = 10, -1
-for c, n in data:
-    if c == "F":
-        x += wpx * n
-        y += wpy * n
-    elif c == "R":
-        for _ in range(n // 90):
-            wpx, wpy = -wpy, wpx
-    elif c == "L":
-        for _ in range(n // 90):
-            wpx, wpy = wpy, -wpx
-    else:
-        wpx, wpy = MOVE[c](wpx, wpy, n)
-print(abs(x) + abs(y))
+print(solve(1, 0, 1))
+print(solve(10, -1, 2))

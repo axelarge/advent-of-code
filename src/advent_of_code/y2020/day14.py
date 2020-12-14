eb6f11802f952363fe30fb99ed59d0352a3a66ed
Miķelis Vindavs <mikelis.vindavs@gmail.com>
2020-12-14 07:55:33 +0200

    Single pass

--- src/advent_of_code/y2020/day14.py
+++ src/advent_of_code/y2020/day14.py
@@ -2,36 +2,28 @@ import re
 
 IN = open("resources/inputs/2020/day14.txt").read()
 
-mem = {}
+mem1 = {}
+mem2 = {}
 mask_and = ~0
-mask_or = 0
-for l in IN.splitlines():
-    if l.startswith("mem"):
-        addr, val = map(int, re.findall(r"\d+", l))
-        mem[addr] = val & mask_and | mask_or
-    else:
-        l = l[7:]
-        mask_and = int(l.replace("X", "1"), 2)
-        mask_or = int(l.replace("X", "0"), 2)
-print(sum(mem.values()))
-
-mem = {}
 mask_or = 0
 floating = []
 for l in IN.splitlines():
     if l.startswith("mem"):
         addr, val = map(int, re.findall(r"\d+", l))
-        addr |= mask_or
+        mem1[addr] = val & mask_and | mask_or
         for i in range(2 ** len(floating)):
-            a1 = addr
+            addr2 = addr | mask_or
             for j, b in enumerate(floating):
                 if i & (1 << j):
-                    a1 |= (1 << b)
+                    addr2 |= (1 << b)
                 else:
-                    a1 &= ~(1 << b)
-            mem[a1] = val
+                    addr2 &= ~(1 << b)
+            mem2[addr2] = val
     else:
         l = l[7:]
+        mask_and = int(l.replace("X", "1"), 2)
         mask_or = int(l.replace("X", "0"), 2)
         floating = list(reversed([36 - i - 1 for i, c in enumerate(l) if c == "X"]))
-print(sum(mem.values()))
+
+print(sum(mem1.values()))
+print(sum(mem2.values()))

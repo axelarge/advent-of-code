0d8fefada0c2dc8d54d64daa0ca302c55f1ae393
Miķelis Vindavs <mikelis.vindavs@gmail.com>
2020-12-15 12:13:01 +0200

    Make 2020.15 5x faster (pypy only)

--- src/advent_of_code/y2020/day15.py
+++ src/advent_of_code/y2020/day15.py
@@ -1,11 +1,17 @@
 data = list(map(int, open("resources/inputs/2020/day15.txt").read().split(",")))
-spoken = {n: i for i, n in enumerate(data)}
+spoken = [-1] * 2 ** 25
+for i, n in enumerate(data):
+    spoken[n] = i
 n = data[-1]
 prev = len(data) - 1
 for i in range(prev + 1, 30_000_000):
     if i == 2020:
         print(n)
     n = i - 1 - prev
-    prev = spoken.get(n, i)
+    while len(spoken) <= n:
+        spoken.append(-1)
+    prev = spoken[n]
+    if prev == -1:
+        prev = i
     spoken[n] = i
 print(n)

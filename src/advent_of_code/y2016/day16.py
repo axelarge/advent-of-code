53317bc899f8c5bb1658c8591cd7b5d2be5d0d4e
Miķelis Vindavs <mikelis.vindavs@gmail.com>
2020-12-25 22:45:01 +0200

    make it much faster

--- src/advent_of_code/y2016/day16.py
+++ src/advent_of_code/y2016/day16.py
@@ -1,10 +1,8 @@
 def solve(data, n):
     while len(data) < n:
         data = data + "0" + data[::-1].translate(str.maketrans("01", "10"))
-    data = data[:n]
-    while len(data) % 2 == 0:
-        data = [str(int(data[i] == data[i + 1])) for i in range(0, len(data), 2)]
-    return "".join(data)
+    chunk_size = n & -n
+    return "".join(str(1 - data[i:i + chunk_size].count("1") % 2) for i in range(0, n, chunk_size))
 
 seed = open("resources/inputs/2016/day16.txt").read().strip()
 print(solve(seed, 272))

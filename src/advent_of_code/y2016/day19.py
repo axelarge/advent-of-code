6bd57a223b8dd4a090f0d4d54f3ce8eba6ff322b
Miķelis Vindavs <mikelis.vindavs@gmail.com>
2020-12-27 19:00:01 +0200

    simplify some more

--- src/advent_of_code/y2016/day19.py
+++ src/advent_of_code/y2016/day19.py
@@ -1,14 +1,8 @@
+from math import log
+
 N = int(open("resources/inputs/2016/day19.txt").read())
 
 print(int(bin(N)[3:] + "1", 2))
 
-after = [(i + 1) % N for i in range(N)]
-oppositeL = N // 2 - 1
-i = 0
-while N > 1:
-    after[oppositeL] = after[after[oppositeL]]
-    if N % 2:
-        oppositeL = after[oppositeL]
-    i = after[i]
-    N -= 1
-print(i + 1)
+z = N - 3 ** int(log(N, 3))
+print(z + max(0, (z * 2) - N) if z else N)

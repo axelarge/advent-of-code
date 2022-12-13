e964ba17ffcd7f3359d8f5d48cb0f9e3f27d8386
Miķelis Vindavs <mikelis.vindavs@gmail.com>
2022-12-13 21:13:23 +0200

    golf

--- src/advent_of_code/y2022/day12.clj
+++ src/advent_of_code/y2022/day12.clj
@@ -10,25 +10,22 @@
 (defn normalize [v]
   (case v \S \a \E \z v))
 
-(defn find-pos [grid pred]
-  (reduce-grid (fn [_ x y v] (when (pred v) (reduced [y x])))
-               nil
-               grid))
-
-(defn can-go? [grid from to]
+(defn can-go? [grid to from]
   (when-let [from-val (normalize (get-in grid from))]
     (when-let [to-val (normalize (get-in grid to))]
       (>= (int from-val) (dec (int to-val))))))
 
-(defn solve [input start end? can-go?]
+(defn solve [input end?]
   (let [grid (parse input)]
-    (dijkstra (find-pos grid #{start})
+    (dijkstra (reduce-grid (fn [_ x y v] (when (= \E v) (reduced [y x])))
+                           nil
+                           grid)
               #(end? (get-in grid %))
               #(->> (neighbors4 %)
                     (filter (partial can-go? grid %))))))
 
 (defn solve1 [input]
-  (solve input \S #{\E} can-go?))
+  (solve input #{\S}))
 
 (defn solve2 [input]
-  (solve input \E #{\S \a} (fn [g from to] (can-go? g to from))))
+  (solve input #{\S \a}))

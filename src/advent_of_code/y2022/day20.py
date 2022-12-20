f65396be19ad9af5980c809c06d0dcf8cb189248
Miķelis Vindavs <mikelis.vindavs@gmail.com>
2022-12-20 09:13:41 +0200

    2022.20

---
+++ src/advent_of_code/y2022/day20.py
@@ -0,0 +1,30 @@
+def mix(data, times=1):
+    data = list(enumerate(data))
+    l = len(data)
+    res = data[:]
+    for _ in range(times):
+        for xx in data:
+            i = res.index(xx)
+            x = xx[1]
+            ii = (i + x) % (l - 1)
+            del res[i]
+            res.insert(ii, xx)
+            # for j in range(x % (l - 1)):
+            #     a = (i + j) % l
+            #     b = (i + j + 1) % l
+            #     res[a], res[b] = res[b], res[a]
+    mixed = [xx[1] for xx in res]
+    i = mixed.index(0)
+    return sum(mixed[(i + d) % l] for d in [1000, 2000, 3000])
+
+
+F = open("resources/inputs/2022/day20.txt").read().splitlines()
+data = list(map(int, F))
+
+part1 = mix(data)
+print(part1)
+assert part1 == 3700
+
+part2 = mix([x * 811589153 for x in data], times=10)
+print(part2)
+assert part2 == 10626948369382

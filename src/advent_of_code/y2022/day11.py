ff31838aa8aa2b1fc7e5c8606841ae7288f7c2ec
Miķelis Vindavs <mikelis.vindavs@gmail.com>
2022-12-25 16:31:07 +0200

    cleanup

--- src/advent_of_code/y2022/day11.py
+++ src/advent_of_code/y2022/day11.py
@@ -6,29 +6,26 @@ F = open("resources/inputs/2022/day11.txt").read()
 monkeys = []
 for raw in F.split("\n\n"):
     lines = raw.splitlines()
-    monkeys.append((tuple(map(int, re.findall(r"\d+", lines[1]))),
-                    [int(x) if x.isnumeric() else x for x in lines[2].split(" = ")[1].split()],
-                    int(re.search(r"\d+", lines[3])[0]),
-                    int(re.search(r"\d+", lines[4])[0]),
-                    int(re.search(r"\d+", lines[5])[0])))
-
-g = prod(m[2] for m in monkeys)
-
+    monkeys.append((list(map(int, re.findall(r"\d+", lines[1]))),
+                    [int(x) if x.isnumeric() else x for x in lines[2].split()[-3:]],
+                    *(int(re.search(r"\d+", line)[0]) for line in lines[3:])))
+M = prod(m[2] for m in monkeys)
 
 def solve(part1):
-    inventory = [list(m[0]) for m in monkeys]
+    inventory = [m[0][:] for m in monkeys]
     times = [0] * len(monkeys)
     for _ in range(20 if part1 else 10000):
         for i, (_, ops, test, true, false) in enumerate(monkeys):
             items = inventory[i]
             for item in items:
                 times[i] += 1
-                a = item if ops[0] == "old" else ops[0]
-                b = item if ops[2] == "old" else ops[2]
-                item = a * b if ops[1] == "*" else a + b
+                a, op, b = ops
+                a = item if a == "old" else a
+                b = item if b == "old" else b
+                item = a * b if op == "*" else a + b
                 if part1:
                     item = item // 3
-                item = item % g
+                item = item % M
                 inventory[false if item % test else true].append(item)
             items.clear()
     return prod(sorted(times)[-2:])

e02c9a39bbfc8308f5debb9e9c209d723cc3c3b9
Miķelis Vindavs <mikelis.vindavs@gmail.com>
2022-12-25 21:19:57 +0200

    2022.19 faster by forwarding to next bot

--- src/advent_of_code/y2022/day19.py
+++ src/advent_of_code/y2022/day19.py
@@ -1,6 +1,6 @@
 import functools
 import re
-from math import prod
+from math import prod, ceil
 
 
 def max_geos(blueprint, time):
@@ -16,12 +16,7 @@ def max_geos(blueprint, time):
         if geo + geo_bots * t + (t * (t - 1)) // 2 <= best_ever:
             return 0
 
-        if t == 1:
-            best = geo + geo_bots
-            best_ever = max(best, best_ever)
-            return best
-
-        # geo bot is best bot
+        # build geo bot now if possible
         if ore >= geo_cost_ore and obs >= geo_cost_obs:
             return inner(t - 1,
                          ore + ore_bots - geo_cost_ore,
@@ -33,56 +28,65 @@ def max_geos(blueprint, time):
                          obs_bots,
                          geo_bots + 1)
 
-        if t == 2:
-            best = geo + geo_bots + geo_bots
+        best = geo + t * geo_bots
+
+        # build geo bot next
+        if obs_bots and ore_bots and t > 1:
+            dt = 1 + max(0, ceil((geo_cost_ore - ore) / ore_bots), ceil((geo_cost_obs - obs) / obs_bots))
+            if dt < t:
+                best = max(best, inner(t - dt,
+                                       ore + dt * ore_bots - geo_cost_ore,
+                                       clay + dt * clay_bots,
+                                       obs + dt * obs_bots - geo_cost_obs,
+                                       geo + dt * geo_bots,
+                                       ore_bots,
+                                       clay_bots,
+                                       obs_bots,
+                                       geo_bots + 1))
+
+        if t <= 2:
             best_ever = max(best, best_ever)
             return best
 
-        best = 0
-        # build ore bot
-        if ore >= ore_cost_ore and ore_bots < max_ore_cost and t > 2:
-            best = max(best, inner(t - 1,
-                                   ore + ore_bots - ore_cost_ore,
-                                   clay + clay_bots,
-                                   obs + obs_bots,
-                                   geo + geo_bots,
-                                   ore_bots + 1,
-                                   clay_bots,
-                                   obs_bots,
-                                   geo_bots))
-        # build clay bot
-        if ore >= clay_cost_ore and clay_bots < obs_cost_clay and t > 3:
-            best = max(best, inner(t - 1,
-                                   ore + ore_bots - clay_cost_ore,
-                                   clay + clay_bots,
-                                   obs + obs_bots,
-                                   geo + geo_bots,
-                                   ore_bots,
-                                   clay_bots + 1,
-                                   obs_bots,
-                                   geo_bots))
-        # build obsidian bot
-        if ore >= obs_cost_ore and clay >= obs_cost_clay and obs_bots < geo_cost_obs and t > 2:
-            best = max(best, inner(t - 1,
-                                   ore + ore_bots - obs_cost_ore,
-                                   clay + clay_bots - obs_cost_clay,
-                                   obs + obs_bots,
-                                   geo + geo_bots,
-                                   ore_bots,
-                                   clay_bots,
-                                   obs_bots + 1,
-                                   geo_bots))
-        # wasn't able to check every type of bot yet, keep digging
-        if ore < max_ore_cost:
-            best = max(best, inner(t - 1,
-                                   ore + ore_bots,
-                                   clay + clay_bots,
-                                   obs + obs_bots,
-                                   geo + geo_bots,
-                                   ore_bots,
-                                   clay_bots,
-                                   obs_bots,
-                                   geo_bots))
+        # build ore bot next
+        if ore_bots < max_ore_cost and ore_bots:
+            dt = 1 + max(0, ceil((ore_cost_ore - ore) / ore_bots))
+            if dt < t:
+                best = max(best, inner(t - dt,
+                                       ore + dt * ore_bots - ore_cost_ore,
+                                       clay + dt * clay_bots,
+                                       obs + dt * obs_bots,
+                                       geo + dt * geo_bots,
+                                       ore_bots + 1,
+                                       clay_bots,
+                                       obs_bots,
+                                       geo_bots))
+        # build clay bot next
+        if clay_bots < obs_cost_clay and ore_bots and t > 3:
+            dt = 1 + max(0, ceil((clay_cost_ore - ore) / ore_bots))
+            if dt < t:
+                best = max(best, inner(t - dt,
+                                       ore + dt * ore_bots - clay_cost_ore,
+                                       clay + dt * clay_bots,
+                                       obs + dt * obs_bots,
+                                       geo + dt * geo_bots,
+                                       ore_bots,
+                                       clay_bots + 1,
+                                       obs_bots,
+                                       geo_bots))
+        # build obsidian bot next
+        if obs_bots < geo_cost_obs and clay_bots and ore_bots:
+            dt = 1 + max(0, ceil((obs_cost_ore - ore) / ore_bots), ceil((obs_cost_clay - clay) / clay_bots))
+            if dt < t:
+                best = max(best, inner(t - dt,
+                                       ore + dt * ore_bots - obs_cost_ore,
+                                       clay + dt * clay_bots - obs_cost_clay,
+                                       obs + dt * obs_bots,
+                                       geo + dt * geo_bots,
+                                       ore_bots,
+                                       clay_bots,
+                                       obs_bots + 1,
+                                       geo_bots))
         return best
 
     return inner(time, 0, 0, 0, 0, 1, 0, 0, 0)

b4604d61e19a1d4a98c9c19d7f848a2ae2c027f5
Miķelis Vindavs <mikelis.vindavs@gmail.com>
2022-12-25 07:45:01 +0200

    2022.25

---
+++ src/advent_of_code/y2022/day25.py
@@ -0,0 +1,27 @@
+D = {k: v for v, k in enumerate("=-012", -2)}
+
+
+def parse(s):
+    res = 0
+    times = 1
+    for c in s[::-1]:
+        res += times * D[c]
+        times *= 5
+    return res
+
+
+def dump(n):
+    res = []
+    while n:
+        digit = n % 5
+        res.append("012=-"[digit])
+        if digit > 2:
+            n += 5 - digit
+        n //= 5
+    return "".join(res[::-1])
+
+
+F = open("resources/inputs/2022/day25.txt").read().splitlines()
+part1 = dump(sum(map(parse, F)))
+print(part1)
+assert part1 == "2=020-===0-1===2=020"
